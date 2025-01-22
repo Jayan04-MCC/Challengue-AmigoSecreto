8d7d46e627cd9f71c06f8f61b4f9ea85d32690fc
Jayan04-MCC <cacerescubajayan@gmail.com>
2025-01-22 11:31:56 -0500

    modifica con la primera funcion

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 //  Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [];
+
+
+function agregarAmigo() {
+    let name = document.getElementById('amigo').value;
+    if(typeof name !== 'string' || name.trim() === ''){
+        alert('El nombre no es valido');
+        console.log(name);
+    }else{
+    listaAmigos.push(name);
+    console.log(listaAmigos);
+    }
+}
